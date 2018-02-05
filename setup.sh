4d321780988dfe303c3ead32b96a555fd1a826f2
Mateus Pimentel <mateuspimentel@gmail.com>
2018-02-04 21:06:51 -0300

    Completing last commit

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-apt-get install --yes --force-yes debootstrap syslinux isolinux squashfs-tools genisoimage
+apt-get install --yes --force-yes debootstrap syslinux isolinux squashfs-tools genisoimage 
 mkdir -p chroot
 debootstrap --arch=i386 --variant=minbase stretch chroot http://ftp.us.debian.org/debian/
 chroot chroot /spectre/install.sh
+
 mkdir -p image/
 mkdir -p image/{live,isolinux}
 cp chroot/boot/isolinux.cfg image/isolinux/isolinux.cfg
 (  cp chroot/boot/vmlinuz* image/live/vmlinuz1 && \
     cp chroot/boot/initrd* image/live/initrd1
 )
+git clone https://github.com/Neohapsis/creddump7.git chroot/spectre/creddump7
+git clone https://github.com/SpiderLabs/Responder.git chroot/spectre/Responder
 (cd image/ && \
 	cp /usr/lib/ISOLINUX/isolinux.bin isolinux/ && \
 	cp /usr/lib/syslinux/modules/bios/menu.c32 isolinux/ && \
@@ -20,7 +23,6 @@ cp chroot/boot/isolinux.cfg image/isolinux/isolinux.cfg
 	cp /usr/lib/syslinux/modules/bios/libgpl.c32 isolinux/ && \
 	cp /usr/share/misc/pci.ids isolinux/ \
 )
-pwd 
 rm -f image/live/filesystem.squashfs scarlet-spectre.iso
 mksquashfs chroot image/live/filesystem.squashfs -e boot
 genisoimage -rational-rock \
